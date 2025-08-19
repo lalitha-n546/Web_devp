5e71d38f9ce26d6f68929d52dad2842d6ddc66be
Lalitha .N <lalitha12m143@gmail.com>
2025-08-19 21:57:12 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number 1:");
+        int num1 = in.nextInt();
+        System.out.println("Enter number 2:");
+        int num2= in.nextInt();
+        System.out.println("The sum is: " + (num1+ num2));
+
+    }
+}
